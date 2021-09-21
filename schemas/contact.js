feba667376c0c32e1f56210f4267121bddfc375b
Elena <e.mandrovska@gmail.com>
2021-09-21 14:15:34 +0300

    chenge regEx for joi validation

--- schemas/contact.js
+++ schemas/contact.js
@@ -1,9 +1,9 @@
 const Joi = require('joi')
 
 const contactSchema = Joi.object({
-  name: Joi.string().alphanum().min(3).required(),
+  name: Joi.string().regex(/^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u).required(),
   email: Joi.string().email({ minDomainSegments: 2 }).required(),
-  phone: Joi.string().min(6).pattern(/^[0-9]+$/).required(),
+  phone: Joi.string().regex(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/).required(),
 })
 
 module.exports = contactSchema
