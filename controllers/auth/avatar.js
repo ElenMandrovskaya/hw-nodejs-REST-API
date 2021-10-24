4b6aea74afb9cea344fdd0ee54e20531418e67bd
Elena <e.mandrovska@gmail.com>
2021-10-24 21:30:39 +0300

    added jimp

--- controllers/auth/avatar.js
+++ controllers/auth/avatar.js
@@ -1,13 +1,16 @@
 const fs = require('fs/promises')
+const Jimp = require('jimp')
 const path = require('path')
 const { User } = require('../../models')
 
 const avatarDir = path.join(__dirname, '../../', 'public/avatars')
 
-const avatars = async (req, res) => {
+const avatar = async (req, res) => {
   const { originalname, path: tempName } = req.file
   try {
     const [extention] = originalname.split('.').reverse()
+    const originalFile = await Jimp.read(tempName)
+    await originalFile.resize(250, 250).writeAsync(tempName)
     const newFileName = `user_${req.user._id}.${extention}`
     const resultStorage = path.join(avatarDir, newFileName)
     await fs.rename(tempName, resultStorage)
@@ -25,4 +28,4 @@ const avatars = async (req, res) => {
   }
 }
 
-module.exports = avatars
+module.exports = avatar
