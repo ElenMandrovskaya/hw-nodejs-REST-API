9f52c005c1c0cb85b8d44f2e37015255c8462d3d
Elena <e.mandrovska@gmail.com>
2021-10-26 18:13:09 +0300

    verify route and cntrl

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,11 +3,13 @@ const signIn = require('./signIn')
 const signOut = require('./signOut')
 const getCurrent = require('./getCurrent')
 const avatars = require('./avatar')
+const verify = require('./verify')
 
 module.exports = {
   signUp,
   signIn,
   signOut,
   getCurrent,
-  avatars
+  avatars,
+  verify
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { controllerWrapper, validation, authenticate, uploadFiles } = require('../../middlewares')
 const { joiSchema } = require('../../models/user')
-const { signUp, signIn, signOut, getCurrent, avatars } = require('../../controllers/auth')
+const { signUp, signIn, signOut, getCurrent, avatars, verify } = require('../../controllers/auth')
 
 const router = express.Router()
 
@@ -14,5 +14,7 @@ router.get('/signout', authenticate, controllerWrapper(signOut))
 router.get('/current', authenticate, controllerWrapper(getCurrent))
 
 router.patch('/avatar', authenticate, uploadFiles.single('avatar'), controllerWrapper(avatars))
+
+router.get('/verify/:verifyToken', controllerWrapper(verify))
 
 module.exports = router
